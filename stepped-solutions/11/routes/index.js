f1b14ed2dcea391e9eb6715971f0fffdbbf5e70b
Eric Reale <ecr5083@gmail.com>
2020-02-02 10:54:44 -0500

    app

--- stepped-solutions/11/routes/index.js
+++ stepped-solutions/11/routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const { catchErrors } = require('../handlers/errorHandlers'); // just importing catchErrors(fn) from errorHandlers.js
 
-router.get('/', storeController.homePage);
+// router.get('/', storeController.getStores);
+// router.get('/stores', storeController.getStores);
 router.get('/add', storeController.addStore);
-router.post('/add', storeController.createStore);
+router.post('/add', catchErrors(storeController.createStore)); // use catchErrors instead of try/catch in async/await
 
 module.exports = router;
